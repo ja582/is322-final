745e12c5e2f724734801f050c48c5a38fde1e997
tarkusdev <mike@tarkus.dev>
2020-05-11 17:45:17 -0500

    Add base components

---
+++ src/components/MovieItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MovieItem() {
+  return (
+    <div>
+      Movie Item for the Movie List
+    </div>
+  );
+}
+
+export default MovieItem;
---
+++ src/components/MovieList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MovieList() {
+  return (
+    <div>
+      List of Movies using MoveItem component
+    </div>
+  );
+}
+
+export default MovieList;
