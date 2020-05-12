e53471d682c6cb021dbed387d9718d0c8da178ad
Joe <ja582@njit.edu>
2020-05-12 03:33:57 -0400

    Feature: MovieCard will be holding movie info pulled from API, for now populated to get it nice and ready for functionality. MovieList will also used for functionality, populated to give an outline.

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -6,9 +6,13 @@ function MovieCard() {
   return (
       <Card style={{ width: '18rem' }}>
           <Card.Body>
-              <Card.Title>Movie Title</Card.Title>
+              <Card.Title>Lost In Translation</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Directed by Sofia Coppola</Card.Subtitle>
               <Card.Text>
-                  movie plot
+                  "Two lost souls visiting Tokyo -- the young, neglected wife of a photographer and a washed-up movie star shooting a TV commercial -- find an odd solace and pensive freedom to be real in each other's company, away from their lives in America."
+              </Card.Text>
+              <Card.Text>
+                  2003 1h 42min 7.4 Rating on IMDb
               </Card.Text>
           </Card.Body>
       </Card>
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -7,18 +7,43 @@ function MovieList() {
           <thead>
           <tr>
               <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Score</th>
           </tr>
           </thead>
           <tbody>
           <tr>
               <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+              <td></td>
+              <td>Lost In Translation</td>
+              <td>10</td>
           </tr>
+          <tr>
+              <td>2</td>
+              <td></td>
+              <td>Fargo</td>
+              <td>10</td>
+          </tr>
+          <tr>
+              <td>3</td>
+              <td></td>
+              <td>Casino</td>
+              <td>2</td>
+          </tr>
+          <tr>
+              <td>4</td>
+              <td></td>
+              <td>Falling Down</td>
+              <td>9</td>
+          </tr>
+          <tr>
+              <td>5</td>
+              <td></td>
+              <td>The Thing</td>
+              <td>10</td>
+          </tr>
+
           </tbody>
       </Table>
   );
