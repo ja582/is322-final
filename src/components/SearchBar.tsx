fda58bfda6ac36973b6ec0ac3c1f4b93ccfeac6c
Joe <ja582@njit.edu>
2020-05-12 03:32:52 -0400

    Basic Search Bar now ready to be used for API functions

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function SearchBar() {
   return (
     <div>
-      SearchBar
+        <Form>
+            <Form.Control type="text" placeholder="Input Movie" />   <Button variant="primary" type="submit"> Submit</Button>
+        </Form>
     </div>
   );
 }
