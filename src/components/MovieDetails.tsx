1ee55bb705b01be87f318d8038034e809484e385
Joe <ja582@njit.edu>
2020-05-13 19:16:58 -0400

    Made MovieDetails more presentable, added poster.

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Movie } from '../store/movies/types';
+import {Link} from "react-router-dom";
+import Image from 'react-bootstrap/Image';
 
 function MovieDetails(movie: Movie) {
   return (
     <div>
-      <p>{movie.id}</p>
-      <p>{movie.title}</p>
+      <Image src ={`https://image.tmdb.org/t/p/w220_and_h330_face${movie.poster_path}`} thumbnail/>
+      <p><h1>{movie.title}</h1></p>
+      <p><h5>Released: {movie.release_date} ★ {movie.vote_average}/10</h5></p>
       <p>{movie.overview}</p>
-      <p>{movie.original_language}</p>
-      <p>{movie.vote_average}</p>
-      <p>{movie.release_date}</p>
-      <p>{movie.adult ? "Yes, 18+" : "No"}</p>
+      <p>Original Language: {movie.original_language}</p>
+      <p>{movie.adult ? "Adults can only watch this." : "This Movie is for all ages."}</p>
     </div>
   );
 }
