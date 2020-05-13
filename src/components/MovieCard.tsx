e60e4fe5692c87669645a326d42705ceda243bf0
tarkusdev <mike@tarkus.dev>
2020-05-13 03:33:39 -0500

    Add MovieCard to Search page

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { Movie } from '../store/movies/types'
 
-function MovieCard() {
+function MovieCard(props: Movie) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
-        <Card.Title>Lost In Translation</Card.Title>
+        <Card.Title>{props.title}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">Directed by Sofia Coppola</Card.Subtitle>
         <Card.Text>
           "Two lost souls visiting Tokyo -- the young, neglected wife of a photographer and a washed-up movie star shooting a TV commercial -- find an odd solace and pensive freedom to be real in each other's company, away from their lives in America."
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,15 +2,31 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Container } from 'react-bootstrap';
 
+import { Movie } from '../store/movies/types';
 import SearchBar from '../components/SearchBar';
 import MovieCard from '../components/MovieCard';
 
 
 function Search() {
-  const [query, setQuery] = useState('');
+  // FOR TESTING PURPOSES ONLY
+  const apiResults = require('../api/ExampleOutput.json').results;
+  // FOR TESTING PURPOSES ONLY
+
+  const [query, setQuery]   = useState('');
+  const [movies, setMovies] = useState(apiResults);
+
+  const updateMovies = (value: string) => {
+    setQuery(value);
+    if (value)
+      setMovies(apiResults);
+  };
+
   return (
     <Container>
-      <SearchBar value={query} setValue={setQuery} />
+      <SearchBar value={query} setValue={updateMovies} />
+      {movies.map((movie: Movie, i: number) => (
+        <MovieCard {...movie} key={i} />
+      ))}
     </Container>
   );
 }
