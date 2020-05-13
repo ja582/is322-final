e5c0819e0e31e5fadb46fd7dd787a48e15f149c1
tarkusdev <mike@tarkus.dev>
2020-05-13 05:20:00 -0500

    Persistent moviesStore storage

--- src/store/movies/reducers.ts
+++ src/store/movies/reducers.ts
@@ -2,48 +2,53 @@ import { HasMovieId } from './helpers';
 import { MoviesState, MoviesActionTypes,
   ADD_MOVIE, ADD_FAVORITE, REMOVE_FAVORITE } from './types'
 
+const initialMovies    = JSON.parse(localStorage.getItem('movies') || '0');
+const initialFavorites = JSON.parse(localStorage.getItem('favorites') || '0');
 
 const initialState: MoviesState = {
-  movies:    [],
-  favorites: [],
+  movies:    Array.isArray(initialMovies) ? initialMovies : [],
+  favorites: Array.isArray(initialFavorites) ? initialFavorites : [],
 };
 
 
 export function MoviesReducer(state = initialState, action: MoviesActionTypes):
   MoviesState
 {
+  let updatedState = state;
+
   switch (action.type) {
     /**
      * Add movie to movies store.
      */
     case ADD_MOVIE:
-      if (HasMovieId(state.movies, action.payload.id))
-        break;
-
-      return {
-        ...state, movies: [ ...state.movies, action.payload ]
-      };
+      if (!HasMovieId(state.movies, action.payload.id))
+        updatedState =  {
+          ...state, movies: [ ...state.movies, action.payload ]
+        };
+      break;
 
     /**
      * Add movie to favorites.
      */
     case ADD_FAVORITE:
-      if (!HasMovieId(state.movies, action.payload.id))
-        break;
-
-      return {
-        ...state, favorites: [ action.payload.id, ...state.favorites ]
-      };
+      if (HasMovieId(state.movies, action.payload.id))
+        updatedState = {
+          ...state, favorites: [ action.payload.id, ...state.favorites ]
+        };
+      break;
 
     /**
      * Remove movie from favorites.
      */
     case REMOVE_FAVORITE:
-      return {
+      updatedState = {
         ...state, favorites: state.favorites.filter(
           (id) => id !== action.payload.id)
       };
+      break;
   }
 
-  return state;
+  localStorage.setItem('movies', JSON.stringify(updatedState.movies));
+  localStorage.setItem('favorites', JSON.stringify(updatedState.favorites));
+  return updatedState;
 }
