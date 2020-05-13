1395d635016824896edd0f02e4644bad7603762a
Joe <ja582@njit.edu>
2020-05-13 18:28:47 -0400

    - Truncating Plot overview on MovieCard

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { Movie } from '../store/movies/types'
 import {Link} from "react-router-dom";
+var TextTruncate = require('react-text-truncate')
 
 function MovieCard(props: Movie) {
   return (
@@ -10,7 +11,12 @@ function MovieCard(props: Movie) {
         <Card.Title><Link to={`/movies/${props.id}`}>{props.title}</Link></Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{props.release_date}</Card.Subtitle>
         <Card.Text>
-          {props.overview}
+          <TextTruncate
+            line={1}
+            element="span"
+            truncateText="..."
+            text={`${props.overview}`}
+          />
         </Card.Text>
         <Card.Text>
            {props.vote_average} Rating on IMDb
