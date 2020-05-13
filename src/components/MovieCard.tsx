d472cf431ccc06d5df4bc92483a6930940bfd5a9
Joe <ja582@njit.edu>
2020-05-13 18:27:16 -0400

    - Added link to movie detail page on MovieCard also

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { Movie } from '../store/movies/types'
+import {Link} from "react-router-dom";
 
 function MovieCard(props: Movie) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title><Link to={`/movies/${props.id}`}>{props.title}</Link></Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{props.release_date}</Card.Subtitle>
         <Card.Text>
           {props.overview}
